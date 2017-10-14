eee917aac046fb3675326c21abbbfbe341ac1d80
Gouresh Phadke <gauresh.phadke@yahoo.com>
2017-10-14 14:27:22 +0530

    Include the custom js file in the layout page itself.

--- PracticeCexV2.Web/Scripts/CustomScripts/PracticeCexV2.js
+++ PracticeCexV2.Web/Scripts/CustomScripts/PracticeCexV2.js
@@ -1,22 +1,3 @@
 ﻿$(document).ready(function () {
-    alert('test');
-    //window.onclick = function (e) {
-    //    if ($(e.target).parents('.glyphiconIconContainerUserProfile').length > 0) {
 
-    //    }
-    //    else {
-    //        if ($('#ulTopNavDropDownList').hasClass('forceDisplayInlineBlock')) {
-    //            $('#ulTopNavDropDownList').removeClass('forceDisplayInlineBlock');
-    //        }
-    //    }
-    //};
-});
-
-$('.glyphiconIconContainerUserProfile').off('click').on('click', function () {
-    //if (!$('#ulTopNavDropDownList').hasClass('forceDisplayInlineBlock')) {
-    //    $('#ulTopNavDropDownList').addClass('forceDisplayInlineBlock');
-    //}
-    //else {
-    //    $('#ulTopNavDropDownList').removeClass('forceDisplayInlineBlock');
-    //}
 });
